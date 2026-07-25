d950062899bceb9bd5eb73e1be60f6344ac44b76
Tholwana <mabuyabt@gmail.com>
2026-07-25 16:35:31 +0200

    fixed page reload bug

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+};
+
+window.addEventListener("load", () => {
+    window.scrollTo(0, 0);
+});
+
+
+
 let lastScroll=0;
 
 window.addEventListener("scroll",()=>{
